e96972898e7304d15d4157a10fe1cf7a80e3e83d
jhShin <sinmagic123@gmail.com>
2025-01-03 15:21:25 +0900

    member & address_book 테이블 수정 / 배송지 작업 / 카멜케이스 리펙토링

--- src/main/resources/static/sql/delivery.sql
+++
@@ -1,9 +0,0 @@
-create table delivery
-(
-    num                   int primary key auto_increment,
-    member_id             varchar(20) not null,
-    shipping_address_name varchar(50) not null,
-    receiver              varchar(20) not null,
-    phone                 varchar(20) not null,
-    address               varchar(50) not null
-);
