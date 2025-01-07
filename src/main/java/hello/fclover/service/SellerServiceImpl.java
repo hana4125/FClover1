80f859972b0540154dde70bb36af0e66e845bc5b
yong-un-jun <junyu0409@naver.com>
2025-01-07 12:50:40 +0900

    goodsSingleInsert 추가

--- src/main/java/hello/fclover/service/SellerServiceImpl.java
+++ src/main/java/hello/fclover/service/SellerServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.fclover.service;
 
+import hello.fclover.domain.Goods;
 import hello.fclover.mybatis.mapper.SellerMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +12,8 @@ public class SellerServiceImpl implements SellerService {
 
     private final SellerMapper dao;
 
+    @Override
+    public void goodsSingleInsert(Goods goods) {
+        dao.goodsSingleInsert(goods);
+    }
 }
