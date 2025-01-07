0096b63f51ea3a465dcd9e6cbd7c09dc1a1ea83f
yong-un-jun <junyu0409@naver.com>
2025-01-07 12:50:07 +0900

    productprocess 수정중

--- src/main/java/hello/fclover/controller/SellerController.java
+++ src/main/java/hello/fclover/controller/SellerController.java
@@ -11,6 +11,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 
 @Slf4j
 @Controller
@@ -31,15 +33,20 @@ public class SellerController {
 
     @PostMapping("/addSingleProductProcess")
     public String addSingleProductProcess(Goods goods,
-                                          @RequestParam("goods_image") MultipartFile[] files,
-                                          Model model) {
+                                          HttpServletRequest request,
+                                          @RequestParam("goods_image") List<MultipartFile> files) {
+        sellerService.goodsSingleInsert(goods);
 
 
-        // 3. 결과 모델에 추가
-        model.addAttribute("message", "상품이 성공적으로 등록되었습니다!");
+            for (MultipartFile file : files) {
+                // 파일 이름 가져오기
+                String fileName = file.getOriginalFilename();
 
-        // 4. 성공 페이지로 리다이렉트 또는 결과 반환
-        return "redirect:/seller/productDetail"; // 성공 후 상세 페이지로 이동
+                System.out.println(file);
+            }
+
+
+        return "seller/sellerAddSingleProduct"; // 성공 후 상세 페이지로 이동
     }
 
     @GetMapping("/productDetail")
