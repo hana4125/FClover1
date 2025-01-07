399633dbfa29893e3647769f9c5c4e4fe71cc4d7
jw0321 <digneotn123@gmail.com>
2025-01-07 18:17:46 +0900

    SQL 문에 따른 VO 클래스 생성

--- src/main/java/hello/fclover/domain/Goods.java
+++ src/main/java/hello/fclover/domain/Goods.java
@@ -1,23 +1,36 @@
 package hello.fclover.domain;
 
+import java.math.BigInteger;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class Goods {
-    // 임시로 ERD 대로 생성
-    private int goods_no;               // 상품 번호
-    private String seller_id;           // 판매자 아이디
-    private int cate_no;                // 카테고리 번호
-    private String goods_name;          // 상품 명
-    private String goods_content;       // 상품 설명
-    private int goods_price;            // 상품 가격
-    private String goods_writer;        // 상품 저자
-    private String seller_company;      // 출판사
-    private LocalDate goods_create_at;  // 상품 발행일
-    private String goods_image;         // 상품 이미지 링크
-    private int goods_count;            // 상품 총 판매수량
-    private LocalDateTime goods_date;   // 상품 등록일
+    // 임시로 SQL 문 대로 생성
+    private BigInteger goodsNo;        // 상품 키
 
+    private Integer goodsIsbn;         // 상품 번호
+
+    private String sellerId;           // 판매자 아이디
+
+    private Integer cateNo;            // 카테고리 번호
+
+    private String goodsName;          // 상품 명
+
+    private String goodsContent;       // 상품 설명
+
+    private int goodsPrice;            // 상품 가격
+
+    private String goodsWriter;        // 상품 저자
+
+    private String sellerCompany;      // 출판사
+
+    private LocalDate goodsCreateAt;   // 상품 발행일
+
+    private String goodsImage;         // 상품 이미지 링크
+
+    private int goodsCount;            // 상품 총 판매수량
+
+    private LocalDateTime goodsDate;   // 상품 등록일
 }
