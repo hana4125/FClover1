7e334b1733e18a0f7e317575970106c3e6458bc7
jw0321 <digneotn123@gmail.com>
2025-01-07 17:57:53 +0900

    header 에서 상세 검색으로 넘어가는 주소 지정 컨트롤러에서 상세 검색으로 넘어가는 매핑 설정 상세 검색 부트스트랩 적용

--- src/main/java/hello/fclover/controller/SearchController.java
+++ src/main/java/hello/fclover/controller/SearchController.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -16,13 +15,10 @@ public class SearchController {
 
     private final SearchService searchService;
 
-    @GetMapping("/")
-    public ModelAndView simpleSearch(String keyword) {
-        ModelAndView mv = new ModelAndView();
-
-        mv.setViewName("user/userSearch");
-        mv.addObject("keyword", keyword);
-        return mv;
+    @GetMapping("/detailSearch")
+    public String detailSearch() {
+        return "user/userDetailSearch";
     }
+
 
 }
