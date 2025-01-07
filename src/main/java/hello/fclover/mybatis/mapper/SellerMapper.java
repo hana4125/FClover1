542072ef3e3345ef0fc0dabbec2026a8b11ca29d
yong-un-jun <junyu0409@naver.com>
2025-01-07 12:50:26 +0900

    goodsSingleInsert 추가

--- src/main/java/hello/fclover/mybatis/mapper/SellerMapper.java
+++ src/main/java/hello/fclover/mybatis/mapper/SellerMapper.java
@@ -1,8 +1,10 @@
 package hello.fclover.mybatis.mapper;
 
+import hello.fclover.domain.Goods;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface SellerMapper {
 
+    void goodsSingleInsert(Goods goods);
 }
