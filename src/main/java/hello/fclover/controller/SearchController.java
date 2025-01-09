341d576b117570a80a1b2fe60a60c5cb8e2ba963
jw0321 <digneotn123@gmail.com>
2025-01-09 17:49:47 +0900

    [header-search-feature] feat : userSearchResult.html 생성
    검색 결과 표시하는 페이지 생성

--- src/main/java/hello/fclover/controller/SearchController.java
+++ src/main/java/hello/fclover/controller/SearchController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -15,10 +16,18 @@ public class SearchController {
 
     private final SearchService searchService;
 
-    @GetMapping("/detailSearch")
+    @GetMapping("/searchDetail")
     public String detailSearch() {
-        return "user/userDetailSearch";
+        return "user/userSearchDetail";
     }
 
+    @GetMapping("/searchDetailResult")
+    public ModelAndView searchDetailResult(
+            String cname, String chrcDetail, String pbcmDetail, String repKeyword, String cate, String repKeywordTarget
+    ) {
+        ModelAndView mv = new ModelAndView();
 
+        mv.setViewName("user/userSearchResult");
+        return mv;
+    }
 }
