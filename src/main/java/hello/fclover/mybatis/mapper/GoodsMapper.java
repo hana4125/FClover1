ac7bfae6c46485cff80f82a2457c2edf95b94e9b
yong-un-jun <junyu0409@naver.com>
2025-01-15 15:37:00 +0900

    [add-single-product] feat : 상품 정보 등록 추가

--- src/main/java/hello/fclover/mybatis/mapper/GoodsMapper.java
+++ src/main/java/hello/fclover/mybatis/mapper/GoodsMapper.java
@@ -1,8 +1,13 @@
 package hello.fclover.mybatis.mapper;
 
+
+import hello.fclover.domain.Goods;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface GoodsMapper {
 
+    int goodsInsertText(Goods goods);
+
+    int goodsNoselect(String sellerId, String goodsName);
 }
