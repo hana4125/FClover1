2c7134b147cbafc8f1ebd9ac4bb4b51cc66aae2c
jhShin <sinmagic123@gmail.com>
2025-01-16 15:46:34 +0900

    git 충돌

--- src/main/java/hello/fclover/config/SecurityConfig.java
+++ src/main/java/hello/fclover/config/SecurityConfig.java
@@ -1,10 +1,7 @@
 package hello.fclover.config;
 
 import hello.fclover.oauth2.service.CustomOAuth2UserService;
-import hello.fclover.security.LoginFailHandler;
-import hello.fclover.security.LoginSuccessHandler;
-import hello.fclover.security.SellerLoginFailHandler;
-import hello.fclover.security.SellerLoginSuccessHandler;
+import hello.fclover.security.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +12,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-    private final DataSource dataSource;
     private final LoginFailHandler loginFailHandler;
     private final LoginSuccessHandler loginSuccessHandler;
-
     private final CustomOAuth2UserService customOAuth2UserService;
 
     @Bean
@@ -55,11 +48,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                //.securityMatcher("/inquiry/**", "/member/**")
                 .formLogin((formLogin) -> formLogin.loginPage("/member/login")
                         .loginProcessingUrl("/member/loginProcess")
                         .usernameParameter("memberId")
                         .passwordParameter("password")
-                        .successHandler(loginSuccessHandler)
                         .successHandler(loginSuccessHandler)
                         .failureHandler(loginFailHandler))
                 .authorizeHttpRequests(authorize -> authorize
@@ -70,17 +63,19 @@ public class SecurityConfig {
                         .requestMatchers("/inquiry/question/**").hasAnyRole("MEMBER","ADMIN")
                         .requestMatchers("/member/**").hasRole("MEMBER")
                         .anyRequest().authenticated()
+
                 )
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/oauth2/authorization/{registrationId}")
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))
-                        .defaultSuccessUrl("/")
+                        .defaultSuccessUrl("/", true)
                 )
                 .logout((lo) -> lo.logoutUrl("/member/logout")
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                 );
+
 
         return http.build();
     }
