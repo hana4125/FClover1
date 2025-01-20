32faae380305c33e040e87a3fbaea7d472bc90de
yong-un-jun <junyu0409@naver.com>
2025-01-20 23:49:55 +0900

    [add-single-product] feat : S3 파일 업로드 초안 추가

--- src/main/java/hello/fclover/mybatis/mapper/GoodsImageMapper.java
+++ src/main/java/hello/fclover/mybatis/mapper/GoodsImageMapper.java
@@ -3,7 +3,12 @@ package hello.fclover.mybatis.mapper;
 import hello.fclover.domain.GoodsImage;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface GoodsImageMapper {
     void goodsInsertImage(GoodsImage goodsImage);
+
+    List<Map<String, String>> findAllGoodsImage(Long goodsNo);
 }
