bfac2917cad8fcd82b1f1f07993fddb5d13ed332
ayoung <ay640809@gmail.com>
2025-01-20 17:34:19 +0900

    [inquiry] Update : 공지,질문 수정

--- src/main/resources/static/sql/qna.sql
+++ src/main/resources/static/sql/qna.sql
@@ -2,7 +2,7 @@ CREATE TABLE QnA(
     q_no int,
     q_create_at date,
     member_id varchar(20),
-    q_response_at date,
+    q_response_at date, #답변일
     q_name varchar(30),
     q_type varchar(20),
     q_file varchar(255),
