a7a942534ded6a0c97689f5ec696609df8c5f880
jhShin <sinmagic123@gmail.com>
2025-01-21 10:19:32 +0900

    feat: 도메인 변수 타입 통일 진행중

--- src/main/java/hello/fclover/domain/AddressBook.java
+++ src/main/java/hello/fclover/domain/AddressBook.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public class AddressBook {
 
     private Long addressNo;
-    private long memberNo;
+    private Long memberNo;
     private String addressName;
     private String recipientName;
     private String phoneNumber;
