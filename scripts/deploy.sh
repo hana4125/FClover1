9b15533f92bc95293fd0d3eea9c3e9e6dab49876
hana4125 <as02268@naver.com>
2025-01-22 11:26:16 +0900

    배포테스트13

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-REPOSITORY=/home/ubuntu/FClover
+REPOSITORY=/home/ubuntu/fclover
 cd $REPOSITORY
 
 APP_NAME=FClover
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+JAR_NAME=$(ls $REPOSITORY/target/ | grep 'SNAPSHOT.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/target/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
