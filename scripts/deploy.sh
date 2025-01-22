d7869a16c3c597f07a03c39582decc77035b63b2
hana4125 <as02268@naver.com>
2025-01-22 12:01:24 +0900

    배포테스트16

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,5 +20,6 @@ else
 fi
 
 echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+#nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
 #nohup java -jar $JAR_PATH > /home/ubuntu/deploy.log 2>&1 &
