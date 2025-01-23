83eeff100cffb6ca160b36d98fa181a56c640c3b
hana4125 <as02268@naver.com>
2025-01-23 16:56:31 +0900

    [delivery] fix : 배포찐마지막수정222

--- src/main/java/hello/fclover/config/WebMvcConfig.java
+++ src/main/java/hello/fclover/config/WebMvcConfig.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Value("${my.savepath}")
-    private String savepath;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations( savepath  );
-    }
+//    @Value("${my.savepath}")
+//    private String savepath;
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/upload/**").addResourceLocations( savepath  );
+//    }
 }
