5fbfc04f2e3f88b564f9fbd1c8f9e564c5faedd5
jw0321 <digneotn123@gmail.com>
2025-01-23 17:28:00 +0900

    chore : RDS 사양에 맞춰 스레드 갯수와 chunk 갯수 조정

--- src/main/java/hello/fclover/config/WebMvcConfig.java
+++ src/main/java/hello/fclover/config/WebMvcConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Value("${my.savepath}")
-    private String savepath;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations( savepath  );
-    }
-}
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    @Value("${my.savepath}")
+//    private String savepath;
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/upload/**").addResourceLocations( savepath  );
+//    }
+//}
