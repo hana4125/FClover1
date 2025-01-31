aaef1cdd937196aa2b1f4798aba0c09ac1b63fb3
yong-un-jun <junyu0409@naver.com>
2025-01-31 11:16:03 +0900

    [goods-Detail] chore : 판매자 상품 검색 프로세스 수정 및 추가

--- src/main/java/hello/fclover/controller/GoodsController.java
+++ src/main/java/hello/fclover/controller/GoodsController.java
@@ -7,6 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/goods")
@@ -20,5 +24,10 @@ public class GoodsController {
         System.out.println("============================================");
         System.out.println("model = " + model.getAttribute("imageList"));
         return "user/userGoodsDetail";
+    }
+    @PostMapping("/SearchGoodsProcess")
+    public List<Goods> searchGoodsProcess(@ModelAttribute Goods goods) {
+        List<Goods> list = new ArrayList<>();
+        return list;
     }
 }
