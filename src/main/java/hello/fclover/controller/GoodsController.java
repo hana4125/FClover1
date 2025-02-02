96a086ffb4b6751e66b901be6a6271cf4adc5603
JW0321 <digneotn123@gmail.com>
2025-02-02 13:31:24 +0900

    [header-search-feature] fix : import 오류 수정

--- src/main/java/hello/fclover/controller/GoodsController.java
+++ src/main/java/hello/fclover/controller/GoodsController.java
@@ -1,12 +1,14 @@
 package hello.fclover.controller;
 
 
+import hello.fclover.domain.Goods;
 import hello.fclover.service.GoodsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
