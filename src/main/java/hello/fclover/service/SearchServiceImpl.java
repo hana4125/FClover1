927db4afd08c651519b45cf1baffb15705dc5c1a
jw0321 <digneotn123@gmail.com>
2025-02-06 10:16:52 +0900

    [header-search-feature] fix : 로그 입력 안되는 문제 해결

--- src/main/java/hello/fclover/service/SearchServiceImpl.java
+++ src/main/java/hello/fclover/service/SearchServiceImpl.java
@@ -14,6 +14,7 @@ import hello.fclover.mybatis.mapper.GoodsMapper;
 import hello.fclover.mybatis.mapper.SearchLogMapper;
 import jakarta.servlet.http.HttpSession;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
@@ -376,6 +377,10 @@ public class SearchServiceImpl implements SearchService {
                 searchLogDTO.setMemberGender("N");
             }
         }
+
+        searchLogDTO.setSearchDatetime(LocalDateTime.now());
+
+        searchLogMapper.insertSearchLog(searchLogDTO);
     }
 
 }
